5587eb1c2955bf42097ec12554e511f562df6d1e
Yuriy108 <sef.yuriy@gmail.com>
2021-04-15 18:35:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    String name="https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Usddownload usd=new Usddownload();
+        usd.execute(name);
+    }
+
+}
+class Usddownload extends AsyncTask<String,Void,String>{
+    URL url=null;
+    HttpURLConnection httpURLConnection=null;
+    StringBuilder result=new StringBuilder();
+
+    @Override
+    protected String doInBackground(String... strings) {
+        try {
+            url=new URL(strings[0]);
+            httpURLConnection=(HttpURLConnection)(url.openConnection());
+            InputStream stream=httpURLConnection.getInputStream();
+            InputStreamReader reader=new InputStreamReader(stream);
+            BufferedReader bufferedReader=new BufferedReader(reader);
+            String line= bufferedReader.readLine();
+            while (line!=null){
+                result.append(line);
+                line=bufferedReader.readLine();
+
+            }
+            return result.toString();
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        Log.d("Myresult",s);
+        try {
+
+            JSONArray array=new JSONArray(s);
+            JSONObject USD=array.getJSONObject(0);
+            String kurs=USD.getString("buy");
+            Log.d("Myresult",kurs);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
